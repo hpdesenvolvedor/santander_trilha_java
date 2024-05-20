5c0d03f731fe181d8c0c669770358af3986dac00
Henrique <nuneshp2@gmail.com>
2024-05-20 00:00:19 -0300

    controle-fluxo

---
+++ 2.Controle-Fluxo/CaixaEletronico.java
@@ -0,0 +1,16 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 24.0;
+
+        if (valorSolicitado < saldo) {
+            saldo = saldo - valorSolicitado;
+            System.out.println("Agora seu saldo é R$: "+saldo);
+            
+        }else{
+            System.out.println("Saldo insuficiente.");
+        }
+        System.out.println("Seu saldo é R$: "+saldo);
+    }
+    
+}
